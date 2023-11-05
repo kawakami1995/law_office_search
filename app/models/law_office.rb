0e1537930800516e4f05975109942d5ca30a6f6a
川上拳太 <kawakamikenta@kawakamikentanoMacBook-Air.local>
2023-11-05 22:21:16 +0900

    [add]law_officeモデル、 reviewモデル、userモデル及び各テーブルを作成
    法律事務所の検索、口コミの投稿・閲覧に必要であるため

---
+++ app/models/law_office.rb
@@ -0,0 +1,2 @@
+class LawOffice < ApplicationRecord
+end
---
+++ db/schema.rb
@@ -0,0 +1,45 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2023_11_05_131049) do
+
+  create_table "law_offices", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "office_name"
+    t.string "representative_lawyer_name"
+    t.string "postal_code"
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "phone_number"
+    t.string "business_hours"
+    t.text "office_url"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "user_id"
+    t.string "law_office_id"
+    t.text "review"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "user_name"
+    t.string "email_address"
+    t.string "password"
+  end
+
+end
