5772249cf6a4c1f65556765d5bc1c1d3763d8c5d
川上拳太 <kawakamikenta@kawakamikentanoMacBook-Air.local>
2023-11-07 21:43:34 +0900

    [add]ユーザー登録機能を追加
    口コミを投稿したいユーザー向けにユーザー登録機能を実装しました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @user = current_user
+    @user = User.find_by(id: params[:id])
   end
 
   def new
@@ -8,9 +8,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params.require(:user).permit(:user_name, :email_address, :password))
+    @user = User.new(params.permit(:user_name, :email_address, :password))
     if @user.save
-      redirect_to "/user/index"
+      redirect_to root_path
     else
       render "new"
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'top#index'
-  get '/user/index', to:'users#index'
   get '/user/new', to:'users#new'
-  post '/user/create', to:'users#create'
+  post '/user', to:'users#create'
+  get '/user/:id', to:'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
