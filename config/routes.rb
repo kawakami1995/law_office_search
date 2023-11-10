d6c4446e026bd820948e7aa0b3e55c5d7f8d0d4f
川上拳太 <kawakamikenta@kawakamikentanoMacBook-Air.local>
2023-11-10 11:12:06 +0900

    [add]Deviseを導入
    サイトにユーザー認証機能を導入するためにDeviseをインストールしました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'top#index'
-  get '/user/new', to:'users#new'
-  post '/user/new', to:'users#create', as: 'user_create'
-  get '/user/:id', to:'users#show'
+
+  post '/users/create', to:'users#create'
+  get '/users/:id', to:'users#show'
+
+  get "/users/sign_in",to:"devise/sessions#new"
+  post "/users/sign_in",to:"evise/sessions#create"
+  delete "/users/sign_out",to:"devise/sessions#destroy"
+  get "/users/password/new",to:"devise/passwords#new"
+  get "/users/password/edit",to:"devise/passwords#edit"
+  patch "/users/password",to:"devise/passwords#update"
+  put "/users/password",to:"devise/passwords#update"
+  post "/users/password",to:"devise/passwords#create"
+  get "/users/cancel",to:"devise/registrations#cancel"
+  get "/users/sign_up",to:"devise/registrations#new"
+  get "/users/edit",to:"devise/registrations#edit"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_05_130632) do
+ActiveRecord::Schema.define(version: 2023_11_09_131516) do
 
   create_table "law_offices", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -32,6 +32,18 @@ ActiveRecord::Schema.define(version: 2023_11_05_130632) do
     t.string "user_id"
     t.string "law_office_id"
     t.text "review"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
