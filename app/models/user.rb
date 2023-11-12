3b29a382d0f7ff92aaaf4b298d64751514528232
川上拳太 <kawakamikenta@kawakamikentanoMacBook-Air.local>
2023-11-12 12:12:46 +0900

    [add]ユーザー登録機能を作成
    deviseを使用してユーザー登録機能を作成しました。
    また、ユーザー登録フォームが空欄のまま登録されることを防ぐために、バリデーションを設定しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :user_name, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :password_confirmation, presence: true
 end
