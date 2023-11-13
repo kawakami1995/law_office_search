d672d6530d11b50acc4d97bbc31294f3f9439e40
川上拳太 <kawakamikenta@kawakamikentanoMacBook-Air.local>
2023-11-13 16:52:45 +0900

    [add]ログアウト機能を実装
    ログアウト機能を実装した上で、トップページのヘッダーにログアウトボタンを設置しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   validates :user_name, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :password_confirmation, presence: true, on: :create
 end
