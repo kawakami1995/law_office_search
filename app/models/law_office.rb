b1a0d5a0695fe0d47a50066665c37d98cc353e71
川上拳太 <kawakamikenta@kawakamikentanoMacBook-Air.local>
2023-11-20 14:13:25 +0900

    [update]入力した住所から法律事務所所在地の緯度経度を取得するように変更
    geocoderジェムにより法律事務所登録の際に入力した住所から当該事務所の緯度経度を取得し、各カラムに保存するよう変更しました。
    これにより、法律事務所の新規登録の際に手動で緯度経度を入力する必要がなくなりました。
    そのため、新規登録画面から緯度経度の入力フォームを削除し、緯度経度入力のためのバリデーションも削除しました。

--- app/models/law_office.rb
+++ app/models/law_office.rb
@@ -1,10 +1,10 @@
 class LawOffice < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
   validates :office_name, presence: true
   validates :representative_lawyer_name, presence: true
   validates :postal_code, presence: true
   validates :address, presence: true
-  validates :latitude, presence: true
-  validates :longitude, presence: true
   validates :phone_number, presence: true
   validates :business_hours, presence: true
   validates :office_url, presence: true
