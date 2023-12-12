ac2cf81d9b3804933587b7443cc8846bf8ba89c0
川上拳太 <kawakamikenta@kawakamikentanoMacBook-Air.local>
2023-12-12 15:39:07 +0900

    [add]検索機能を追加
    トップページに事務所名で検索する機能を追加しました

--- app/controllers/law_offices_controller.rb
+++ app/controllers/law_offices_controller.rb
@@ -54,6 +54,6 @@ class LawOfficesController < ApplicationController
 
   def search
     @user = current_user
-    @law_offices = LawOffice.where("address like ? and focus like ?", "#{params[:prefectures]}%", "%#{params[:focus]}%" )
+    @law_offices = LawOffice.where("office_name like ? and address like ? and focus like ?", "%#{params[:office_name]}%", "#{params[:prefectures]}%", "%#{params[:focus]}%" )
   end
 end
