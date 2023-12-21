dc2e731b0bb02c97c4790df3aa1a64f54a848e51
川上拳太 <kawakamikenta@kawakamikentanoMacBook-Air.local>
2023-12-21 19:05:47 +0900

    [update]マイページにお気に入り登録した事務所の一覧を表示
    ユーザーがお気に入り登録した事務所を確認できるように、また、お気に入り登録した事務所のページにすぐアクセスできるようにするために実装しました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
   def account
     @user = current_user
     @reviews = Review.where(user_id: @user.id)
+    @favorites = Favorite.where(user_id: @user.id)
+    favorite_ids = @favorites.pluck(:law_office_id)
+    @law_offices = LawOffice.where(id: favorite_ids).order(:postal_code)
   end
 
   def user_name_edit
