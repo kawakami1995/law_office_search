64dc40c9c5e414c633721d3c825731bd34b43722
川上拳太 <kawakamikenta@kawakamikentanoMacBook-Air.local>
2023-12-23 22:35:54 +0900

    [update]法律事務所詳細ページの処理を修正
    無駄な処理を省くためにcontrollerおよびviewの処理を一部変更しました。

--- app/controllers/law_offices_controller.rb
+++ app/controllers/law_offices_controller.rb
@@ -23,9 +23,7 @@ class LawOfficesController < ApplicationController
     @user = current_user
     @law_office = LawOffice.find_by(id: params[:id])
     @reviews = Review.where(law_office_id: @law_office.id)
-    if @user.nil?
-      @favorite = nil
-    else
+    if @user.present?
       @favorite = Favorite.find_by(user_id: @user.id, law_office_id: @law_office.id)
     end
   end
