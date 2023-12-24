95563d3750a23878ced03c718aba34fb92d49131
川上拳太 <kawakamikenta@kawakamikentanoMacBook-Air.local>
2023-12-24 20:11:48 +0900

    [remove]不要な記述を削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,17 +6,4 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :password_confirmation, presence: true, on: :create
   has_many :favorites, dependent: :destroy
-  # has_many :favorited_law_offices, through: :favorites, source: :law_office
-
-  # def favorite(law_office)
-  #   favorited_law_offices << law_office
-  # end
-
-  # def unfavorite(law_office)
-  #   favorited_law_offices.destroy(law_office)
-  # end
-
-  # def favorited?(law_office)
-  #   favorited_law_offices.include?(law_office)
-  # end
 end
